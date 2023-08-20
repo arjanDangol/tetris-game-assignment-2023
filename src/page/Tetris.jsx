32f193292c79242abb2e3e73439613dcc9a1ff98
Arjan Dangol <arjan.dangol95@gmail.com>
2023-08-20 22:47:21 +1000

    Create stage, display, and button area for the game

--- src/page/Tetris.jsx
+++ src/page/Tetris.jsx
@@ -3,20 +3,26 @@ import Stage from "../components/Stage";
 import Display from "../components/Display";
 import StartButton from "../components/StartButton";
 import { createStage } from "../utils/gameHelper";
+import {
+  StyledTetrisWrapper,
+  StyledTetris,
+} from "../components/styles/StyledTetris";
 
 const Tetris = ({ callback }) => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
-        <div>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
-        </div>
-        <StartButton />
-      </aside>
-    </div>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton />
+        </aside>
+      </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
