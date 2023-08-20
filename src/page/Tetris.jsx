9f64d92aad19ba94d614303723ef1a29e71243c2
Arjan Dangol <arjan.dangol95@gmail.com>
2023-08-20 22:23:25 +1000

    Add stage content and random tetromino generator

--- src/page/Tetris.jsx
+++ src/page/Tetris.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Stage from "../components/Stage";
 import Display from "../components/Display";
 import StartButton from "../components/StartButton";
+import { createStage } from "../utils/gameHelper";
 
 const Tetris = ({ callback }) => {
   return (
     <div>
-      <Stage />
+      <Stage stage={createStage()} />
       <aside>
         <div>
           <Display text="Score" />
