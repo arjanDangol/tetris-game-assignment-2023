4029ec706c8f117b4265551eb9bfd0d63c17955f
Arjan Dangol <arjan.dangol95@gmail.com>
2023-08-21 23:14:28 +1000

    Add custom hooks for stage and player

--- src/page/Tetris.jsx
+++ src/page/Tetris.jsx
@@ -1,24 +1,41 @@
-import React from "react";
-import Stage from "../components/Stage";
-import Display from "../components/Display";
-import StartButton from "../components/StartButton";
-import { createStage } from "../utils/gameHelper";
+import React, { useState } from "react";
+
+// Custom Hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
+
+// Styled components
 import {
   StyledTetrisWrapper,
   StyledTetris,
 } from "../components/styles/StyledTetris";
 
+// Components
+import Stage from "../components/Stage";
+import Display from "../components/Display";
+import StartButton from "../components/StartButton";
+
 const Tetris = ({ callback }) => {
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+  console.log("re-render");
   return (
     <StyledTetrisWrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
-          <div>
-            <Display text="Score" />
-            <Display text="Rows" />
-            <Display text="Level" />
-          </div>
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
           <StartButton />
         </aside>
       </StyledTetris>
